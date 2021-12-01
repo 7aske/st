8ea432bd7c380b6979424bd5a5300aeca1a01039
Nikola Tasic <ntasic7@gmail.com>
2021-12-02 00:07:35 +0100

    fixed font dpi

--- config.h
+++ config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "FiraCode-Regular:pixelsize=16:antialias=true:autohint=true";
-static char *font2[] = { "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
+static char *font = "FiraCode-Regular:size=10:antialias=true:autohint=true";
+static char *font2[] = { "JoyPixels:size=10:antialias=true:autohint=true" };
 static int borderpx = 5;
 
 /*
